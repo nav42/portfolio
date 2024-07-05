94da4cc030d52a4bfb70c2e68e7ee56712206d6f
nav42 <naveednekoo42@gmail.com>
2024-07-05 19:30:49 -0400

    test commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,7 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
-<h1> Counter: {count}</h1>
+<h1> NAV : {count}</h1>
 <button on:click={() => count += 1}>Increment</button>
 <button on:click={() => count -= 1}>Decrement</button>
 
